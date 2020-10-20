13ff9b200ed4d9327af195f6079741b84e0d784f
Rickey <weems505@gmail.com>
2020-10-20 11:25:53 -0500

    Initial springboot app

---
+++ src/main/java/com/example/demo/PyramidBankApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PyramidBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PyramidBankApplication.class, args);
+    }
+
+}
